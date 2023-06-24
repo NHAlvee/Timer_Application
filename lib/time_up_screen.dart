cd378eeb4d07077f779ce63425a271f4304d5059
NHAlvee <73606235+NHAlvee@users.noreply.github.com>
2023-06-24 19:58:17 +0600

    Initial commit

---
+++ lib/time_up_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:just_audio/just_audio.dart';
+
+class TimeUpClass extends StatelessWidget {
+  final AudioPlayer player;
+
+  const TimeUpClass({Key? key, required this.player}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        await player.stop();
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Center(
+          child: Text(
+            "Clock Out",
+            // style: Theme.of(context).textTheme.headlineLarge,
+            style: GoogleFonts.aBeeZee(
+              textStyle: const TextStyle(
+                color: Colors.blue,
+                letterSpacing: .5,
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
